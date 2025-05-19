0f4b04f455703c5f76f7d2e6ae47d2afd3de5e7b
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-05-19 20:26:54 +0200

    attempt to serve style files as webassets

--- ckanext/digitizationknowledge_theme/plugin.py
+++ ckanext/digitizationknowledge_theme/plugin.py
@@ -26,7 +26,7 @@ class DigitizationknowledgeThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "digitizationknowledge_theme")
+        toolkit.add_resource("assets", "ckanext-digitizationknowledge_theme")
 
     
     # IAuthFunctions
