9c6639af2354c3a4dd31659b918cf2682264e236
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-05-21 20:26:07 +0200

    Refactor CSS asset management through webassets

--- ckanext/digitizationknowledge_theme/plugin.py
+++ ckanext/digitizationknowledge_theme/plugin.py
@@ -26,8 +26,8 @@ class DigitizationknowledgeThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "ckanext-digitizationknowledge_theme")
-
+        toolkit.add_resource("assets", "digitizationknowledge_theme")
+    
     
     # IAuthFunctions
 
